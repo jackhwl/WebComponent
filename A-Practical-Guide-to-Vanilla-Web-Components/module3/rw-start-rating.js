957f59583f79f74c62fa94778cc58ea74b8b6035
jackhwl <jackhwl@hotmail.com>
2018-12-28 15:13:13 -0500

    slide out menu component

--- A-Practical-Guide-to-Vanilla-Web-Components/module3/rw-start-rating.js
+++ A-Practical-Guide-to-Vanilla-Web-Components/module3/rw-start-rating.js
@@ -23,62 +23,62 @@ class RwStarRating extends HTMLElement {
     connectedCallback() {
         this._root.innerHTML = `
             <style>
-            :host {
-                width: 4.1em;
-                height: 1em;
-                display: inline-block;
-                overflow: hidden;
-                user-select: none;
-                vertical-align: middle;
-                box-sizing: border-box;
-            }          
-            .container {                  
-            color: #c5c5c5;
-            font-size: 0.8em;
-            line-height: 1em;
-            margin: 0 auto;
-            position: relative;
-            padding: 0;
-            cursor: pointer;
-            }               
-            .container .top {
-            color: #e7bd06;
-            padding: 0;
-            position: absolute;
-            z-index: 1;
-            display: block;
-            top: 0;
-            left: 0;
-            overflow: hidden;
-            width: 0;       
-            }
-            .container:hover .top {
-                display: none;
-            }                             
-            .container .bottom {
-            padding: 0;
-            display: block;
-            position: absolute;
-            top: 0;
-            left: 0;
-            unicode-bidi: bidi-override;
-            direction: rtl;
-            }
-            /* Credit: https://css-tricks.com/star-ratings/ */
-            .container .bottom > span:hover,
-            .container .bottom > span:hover ~ span {               
-            color: #e7bd06;
-            }               
-            :host([disabled]) .container {
-                cursor: inherit;
-            }
-            :host([disabled]) .container .top {
+                :host {
+                    width: 4.1em;
+                    height: 1em;
+                    display: inline-block;
+                    overflow: hidden;
+                    user-select: none;
+                    vertical-align: middle;
+                    box-sizing: border-box;
+                }          
+                .container {                  
+                color: #c5c5c5;
+                font-size: 0.8em;
+                line-height: 1em;
+                margin: 0 auto;
+                position: relative;
+                padding: 0;
+                cursor: pointer;
+                }               
+                .container .top {
+                color: #e7bd06;
+                padding: 0;
+                position: absolute;
+                z-index: 1;
                 display: block;
-            }
-            :host([disabled]) .container .bottom > span:hover,
-            :host([disabled]) .container .bottom > span:hover ~ span {
-                color: inherit;
-            }
+                top: 0;
+                left: 0;
+                overflow: hidden;
+                width: 0;       
+                }
+                .container:hover .top {
+                    display: none;
+                }                             
+                .container .bottom {
+                padding: 0;
+                display: block;
+                position: absolute;
+                top: 0;
+                left: 0;
+                unicode-bidi: bidi-override;
+                direction: rtl;
+                }
+                /* Credit: https://css-tricks.com/star-ratings/ */
+                .container .bottom > span:hover,
+                .container .bottom > span:hover ~ span {               
+                color: #e7bd06;
+                }               
+                :host([disabled]) .container {
+                    cursor: inherit;
+                }
+                :host([disabled]) .container .top {
+                    display: block;
+                }
+                :host([disabled]) .container .bottom > span:hover,
+                :host([disabled]) .container .bottom > span:hover ~ span {
+                    color: inherit;
+                }
             </style>
             <div class="container">
                 <div class="top">
@@ -115,7 +115,6 @@ class RwStarRating extends HTMLElement {
         return ["disabled", "value"];
     }
     attributeChangedCallback(name, oldValue, newValue) {
-        console.log(name + ' changed');
         if (oldValue !== newValue) {
             switch (name) {
                 case "disabled":
