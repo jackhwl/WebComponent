2ba8fd50946d3ebb1bb63946d2be970e5acd5cdc
jackhwl <jackhwl@hotmail.com>
2018-12-31 11:44:22 -0500

    Hijacking Requests jpg file

--- wittr/public/js/sw/index.js
+++ wittr/public/js/sw/index.js
@@ -1,3 +1,11 @@
 self.addEventListener('fetch', function(event) {
-    console.log('Hello 222 333world!!!!!!!');
+    //console.log('Hello 222 333 45s world!!!!!!!');
+    if (event.request.url.endsWith('.jpg')) {
+        event.respondWith(
+            // new Response('<div class="a-winner-is-me"></div>Hello <b>world!</b></div>', {
+            //     headers: {'foo': 'bar', 'Content-Type': 'text/html'}
+            // })
+            fetch('/imgs/dr-evil.gif')
+        );
+    }
 });
