813826994575ca1dcfef497a15771907126ca34c
jackhwl <jackhwl@hotmail.com>
2019-01-13 21:21:51 -0500

    add workbox

--- pwa-series/sw.js
+++ pwa-series/sw.js
@@ -1,15 +1,40 @@
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open("precache-v1").then(cache => {
-            cache.addAll(["/", "app.js"]);
-        })
-    );
-});
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
 
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.response).then(response => {
-            return response || fetch(event.request);
-        })
-    );
-});
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "app.css",
+    "revision": "d41d8cd98f00b204e9800998ecf8427e"
+  },
+  {
+    "url": "app.js",
+    "revision": "e4f46076b3601450174e8578527cdac7"
+  },
+  {
+    "url": "index.html",
+    "revision": "b1221096e624f9ead3cb9d0911f05a3b"
+  },
+  {
+    "url": "workbox-config.js",
+    "revision": "2a5130c5c0adbb75c11ff6636f008fb6"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
