2dc39784b57d6a6745e64150cd8c33c420aadf76
jackhwl <jackhwl@hotmail.com>
2019-01-14 07:31:04 -0500

    add images and manifest init

---
+++ pwa-series/dist/precache-manifest.d5b0f2432d4952bb241c01866f91e0fe.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = [
+  {
+    "revision": "91092ebbddbd749fdfe0d0c4d5024551",
+    "url": "index.html"
+  },
+  {
+    "revision": "bbbd1808471925554be3",
+    "url": "app.js"
+  }
+];
--- pwa-series/dist/sw.js
+++ pwa-series/dist/sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.656e36fd86f4c5e5b9743517dbfdb33d.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("precache-manifest.d5b0f2432d4952bb241c01866f91e0fe.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 console.log("⚙️ Hello from Service Worker");
 
