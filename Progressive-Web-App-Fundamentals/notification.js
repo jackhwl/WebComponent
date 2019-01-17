7cbf09002ffbc48e73fbb5138e16e9e5d444b334
Jack Huang <jackhwl@hotmail.com>
2019-01-17 15:21:28 -0500

    persist/none notification

---
+++ Progressive-Web-App-Fundamentals/notification.js
@@ -0,0 +1,72 @@
+// https://tests.peter.sh/ notification generator
+
+if (Notification.permission === 'granted') {
+    showNotification();
+    return;
+}
+
+if (Notification.permission !== 'denied') {
+    Notification.requestPermission().then(p => {
+        if (p === 'granted') showNotification();
+    });
+}
+
+var n = new Notification('Title', 
+{
+    body: 'body text',
+    badge: '/path/to/image.ext',
+    icon: '/path/to/image.ext',
+    image: 'path/to/image.ext',
+    tag: 'foo',
+    renotity: false,
+    data: { 
+        // obj here
+    },
+    requireInteraction: false,
+    actions: [{
+        action: 'id',
+        title: 'Action Title',
+        icon: '/path/to/image.ext'
+    }],
+    silent: false,
+    sound: '/path/to/audio.ext',
+    vibrate: [200, 100, 200],
+    dir: 'ltr',
+    lang: 'en-US',
+    timestamp: Date.now(),
+});
+
+n.addEventListener('error', evt => {
+    console.error('There was a problem', evt);
+});
+
+n.addEventListener('click', evt => {
+    console.log('Notification clicked!');
+});
+
+n.close();
+
+// persistent Notifications
+
+self.registration.showNotification('Title', {
+    actions: [{ action: 'view', title: 'Action Title', icon: 'plus.png'}],
+    // other options
+});
+
+self.addEventListener('notificationclick', evt => {
+    // clicked on action body not button
+    if (!evt.action) {
+        console.log('Notification clicked!');
+        return;
+    }
+
+    // handle notification action click
+    switch (evt.action) {
+        case 'view':
+            console.log('View action clicked!');
+            break;
+        default:
+            console.warn('${evt.action} action clicked!');
+            break;
+    }
+});
