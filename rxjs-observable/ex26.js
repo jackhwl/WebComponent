b17ccddd71caaa2e4141b0fa69c5d3d222b4447b
jackhwl <jackhwl@hotmail.com>
2019-01-23 07:27:38 -0500

    ex26 Converting from Arrays to Deeper Trees

---
+++ rxjs-observable/ex26.js
@@ -0,0 +1,67 @@
+// Exercise 26: Converting from Arrays to Deeper Trees
+function twentysix() {
+	var lists = [
+			{
+				"id": 5434364,
+				"name": "New Releases"
+			},
+			{
+				"id": 65456475,
+				name: "Thrillers"
+			}
+		],
+		videos = [
+			{
+				"listId": 5434364,
+				"id": 65432445,
+				"title": "The Chamber"
+			},
+			{
+				"listId": 5434364,
+				"id": 675465,
+				"title": "Fracture"
+			},
+			{
+				"listId": 65456475,
+				"id": 70111470,
+				"title": "Die Hard"
+			},
+			{
+				"listId": 65456475,
+				"id": 654356453,
+				"title": "Bad Boys"
+			}
+		],
+		boxarts = [
+			{ videoId: 65432445, width: 130, height:200, url:"http://cdn-0.nflximg.com/images/2891/TheChamber130.jpg" },
+			{ videoId: 65432445, width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/TheChamber200.jpg" },
+			{ videoId: 675465, width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture200.jpg" },
+			{ videoId: 675465, width: 120, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture120.jpg" },
+			{ videoId: 675465, width: 300, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture300.jpg" },
+			{ videoId: 70111470, width: 150, height:200, url:"http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" },
+			{ videoId: 70111470, width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/DieHard200.jpg" },
+			{ videoId: 654356453, width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/BadBoys200.jpg" },
+			{ videoId: 654356453, width: 140, height:200, url:"http://cdn-0.nflximg.com/images/2891/BadBoys140.jpg" }
+		],
+		bookmarks = [
+			{ videoId: 65432445, time: 32432 },
+			{ videoId: 675465, time: 3534543 },
+			{ videoId: 70111470, time: 645243 },
+			{ videoId: 654356453, time: 984934 }
+		];
+
+		return lists. // complete this expression
+			map(list => ({name: list.name, videos: videos.
+						filter(video => video.listId === list.id).
+						concatMap(video => 
+							Array.zip(bookmarks.filter(bookmark => bookmark.videoId === video.id), 
+								boxarts.filter(boxart => boxart.videoId === video.id).
+									reduce((pre, cur) => pre.width * pre.height < cur.width * cur.height ? pre : cur), 
+								(bookmark, boxart) => ({
+									id: video.id, title: video.title, time: bookmark.time, boxart: boxart.url
+								}))
+						)
+					}))
+
+}
+		
