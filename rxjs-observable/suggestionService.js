552b05bc36f898ab1b1a7bab6f37b780c2e7e35d
Jack Huang <jackhwl@hotmail.com>
2019-01-28 07:22:30 -0500

    refine suggestion service

--- rxjs-observable/suggestionService.js
+++ rxjs-observable/suggestionService.js
@@ -3,24 +3,19 @@ angular.module('app')
 
 suggestionService.$inject = ['errorService', 'rx'];
 function suggestionService(errorService, rx) {
-    var fetchFn, paramFn;
-    function currentObserableSuggestions() {
-        var param = paramFn();
-        if (!param.eventTarget.value || param.eventTarget.value.trim()==='') return rx.Observable.of([]);
-
-        return rx.Observable.defer(function() {
-            return fetchFn(param).query().$promise;
-        });
-    }
-
-    function assignFunction(fetchSuggestion, getSuggestionParam) {
-        fetchFn = fetchSuggestion;
-        paramFn = getSuggestionParam;
-    }
 
     this.getObservableSuggestions = function(fetchSuggestion, getSuggestionParam) {
-        assignFunction(fetchSuggestion, getSuggestionParam);
-        return rx.Observable.fromEvent(paramFn().eventTarget, 'keyup').
+        
+        function currentObserableSuggestions() {
+            var param = getSuggestionParam();
+            if (!param.eventTarget.value || param.eventTarget.value.trim()==='') return rx.Observable.of([]);
+    
+            return rx.Observable.defer(function() {
+                return fetchSuggestion(param).query().$promise;
+            });
+        }
+
+        return rx.Observable.fromEvent(getSuggestionParam().eventTarget, 'keyup').
             throttle(250).
             map(function() {
                 return currentObserableSuggestions().
