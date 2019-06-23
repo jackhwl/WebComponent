6530c68fe856165cb9778bc6329d965361e4b0b8
jackhwl <jackhwl@hotmail.com>
2019-06-23 19:04:55 -0400

    observable comment

--- RxJS-Getting-Started/RxBookTracker/index.ts
+++ RxJS-Getting-Started/RxBookTracker/index.ts
@@ -1,6 +1,31 @@
 import { Observable } from 'rxjs';
 import { allBooks } from './data';
 
+/**
+ * if you want to consume the data inside of an observable. Create
+ * an observer object with three handlers. You hand the observer object 
+ * to an observable. the observable pushed data into the observer object
+ * and by extension your callbacks. And it'll call onNext, onError, onComplete
+ * 
+ * subscribe
+ * var subscription = mouseMoves.forEach(
+ *      event => console.log(event),
+ *      error => console.error(error),
+ *      () => console.log("done");
+ * );
+ * * var subscription = mouseMoves.forEach({     // Observer object
+ * *      onNext: event => console.log(event),
+ * *      onError: error => console.error(error),
+ * *      onCompleted: () => console.log("done");
+ * * });
+ * unsubscribe
+ * subscription.dispose();
+ * 
+ * An observer observes an observable
+ * 
+ * An observable is nothing but an object with a forEach method.
+ */
+
 function subscribe3(subscriber) {
     for (let book of allBooks) {
         subscriber.next(book);
