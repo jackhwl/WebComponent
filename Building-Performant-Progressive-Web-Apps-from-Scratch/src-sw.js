55778cbb76414232872759ae47a5b884dbaf76b0
Wenlin Huang <jackhwl@hotmail.com>
2019-07-10 18:49:43 -0400

    Caching Dynamic Content

--- Building-Performant-Progressive-Web-Apps-from-Scratch/src-sw.js
+++ Building-Performant-Progressive-Web-Apps-from-Scratch/src-sw.js
@@ -1,1 +1,6 @@
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+workbox.routing.registerRoute(
+    /https:\/\/pluralsight\-pwa\-scratch\.firebaseio\.com\/.*\.json/,
+  new workbox.strategies.NetworkFirst()
+)
--- Building-Performant-Progressive-Web-Apps-from-Scratch/src/index.js
+++ Building-Performant-Progressive-Web-Apps-from-Scratch/src/index.js
@@ -5,3 +5,13 @@ if ("serviceWorker" in navigator) {
         })
         .catch(error => console.error(error));
 }
+
+fetch("https://pluralsight-pwa-scratch.firebaseio.com/flights.json")
+  .then(response => response.json())
+  .then(data => {
+    let html = "";
+    data.forEach(flight => {
+      html += `<div class="card">${flight.city}</div>`;
+    });
+    document.querySelector("#screen-flights").innerHTML = html;
+  });
