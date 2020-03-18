b6c2d1056087896eb413a83859cd94fb873c3ff6
Wenlin Huang <jackhwl@hotmail.com>
2020-03-18 18:43:49 -0400

    utils

--- design-system-storybook/src/index.js
+++ design-system-storybook/src/index.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import PrimaryButton, { SecondaryButton, TertiaryButton } from './components/Buttons'
 
-const App = () => <h1>Hello world</h1>
 
+
+const App = () => (
+    <div>
+<PrimaryButton>Hello world</PrimaryButton>
+<SecondaryButton>Goodbye world</SecondaryButton>
+<TertiaryButton>Hey world</TertiaryButton>
+
+    </div>
+)
 ReactDOM.render(<App />, document.querySelector("#root"))
