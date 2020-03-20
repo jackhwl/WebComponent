554f57625897fdbb954b55aaa2a0fb2eeba903e5
Wenlin Huang <jackhwl@hotmail.com>
2020-03-20 17:20:41 -0400

    setting-up-dashboard-pages

---
+++ jamstack-intro/dashboard/src/pages/dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const Dashboard = () => {
+    return (
+        <Layout>
+            <p>Todo: create a dashboardcc</p>
+        </Layout>
+    )
+}
+
+export default Dashboard
