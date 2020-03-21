dcc3668b2c780f399775182aa367b9668ab1fff6
Wenlin Huang <jackhwl@hotmail.com>
2020-03-21 12:49:56 -0400

    wiring-up-client-side-routes

--- jamstack-intro/dashboard/src/pages/dashboard.js
+++ jamstack-intro/dashboard/src/pages/dashboard.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { navigate } from 'gatsby'
 import { Router } from '@reach/router'
 import Layout from '../components/layout'
 import Profile from '../components/profile'
@@ -6,7 +7,12 @@ import RouteBase from '../components/route-base'
 import RouteSecret from '../components/route-secret'
 import RouteLogin from '../components/route-login'
 
-const Dashboard = () => {
+const Dashboard = ({ location }) => {
+    useEffect(() => {
+        if (location.pathname.match(/^\/dashboard\/?$/)) {
+            navigate('dashboard/login', {replace: true})
+        }
+    }, [])
     return (
         <Layout>
             <Profile />
