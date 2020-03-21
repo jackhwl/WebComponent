266cbcc60269075b98eafd8d486aac5e1acc52b0
Wenlin Huang <jackhwl@hotmail.com>
2020-03-21 12:21:50 -0400

    setting-up-a-login-route

--- jamstack-intro/dashboard/src/pages/dashboard.js
+++ jamstack-intro/dashboard/src/pages/dashboard.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Router } from '@reach/router'
 import Layout from '../components/layout'
+import Profile from '../components/profile'
+import RouteBase from '../components/route-base'
+import RouteSecret from '../components/route-secret'
+import RouteLogin from '../components/route-login'
 
 const Dashboard = () => {
     return (
         <Layout>
-            <p>Todo: create a dashboardcc</p>
+            <Profile />
+            <Router>
+                <RouteBase path="/dashboard/base" />
+                <RouteSecret path="/dashboard/secret" />
+                <RouteLogin path="/dashboard/login" />
+            </Router>
         </Layout>
     )
 }
