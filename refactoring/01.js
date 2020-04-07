d7de9e0f4864cbbd3a95343e730d76131080719b
Wenlin Huang <jackhwl@hotmail.com>
2020-04-07 10:32:04 -0400

    p67 extract amountFor function

--- refactoring/01.js
+++ refactoring/01.js
@@ -9,24 +9,7 @@ function statement(invoice, plays) {
     }).format
     for (let perf of invoice.performances) {
         const play = plays[perf.playID]
-        let thisAmount = 0
-        switch (play.type) {
-            case 'tragedy':
-                thisAmount = 40000
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30)
-                }
-                break
-            case 'comedy':
-                thisAmount = 30000
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20)
-                }
-                thisAmount += 300 * perf.audience
-                break
-            default:
-                throw new Error(`unknown type: ${play.type}`)
-        }
+        let thisAmount = amountFor(perf, play)
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0)
         // add extra credit for every ten comedy attendees
@@ -42,3 +25,53 @@ function statement(invoice, plays) {
     result += `You earned ${volumeCredits} credits\n`
     return result
 }
+function amountFor(perf, play) {
+    let thisAmount = 0
+    switch (play.type) {
+        case 'tragedy':
+            thisAmount = 40000
+            if (perf.audience > 30) {
+                thisAmount += 1000 * (perf.audience - 30)
+            }
+            break
+        case 'comedy':
+            thisAmount = 30000
+            if (perf.audience > 20) {
+                thisAmount += 10000 + 500 * (perf.audience - 20)
+            }
+            thisAmount += 300 * perf.audience
+            break
+        default:
+            throw new Error(`unknown type: ${play.type}`)
+    }
+    return thisAmount
+}
+let plays = {
+    hamlet: { name: 'Hamlet', type: 'tragedy' },
+    'as-like': { name: 'As You Like It', type: 'comedy' },
+    othello: { name: 'Othello', type: 'tragedy' },
+}
+
+let invoice =
+    //[
+    {
+        customer: 'BigCo',
+        performances: [
+            {
+                playID: 'hamlet',
+                audience: 55,
+            },
+            {
+                playID: 'as-like',
+                audience: 35,
+            },
+            {
+                playID: 'othello',
+                audience: 40,
+            },
+        ],
+    }
+//]
+
+let result = statement(invoice, plays)
+console.log(result)
