f425d3ce55a2093c69115321f24c3efb72e5cb49
Wenlin Huang <jackhwl@hotmail.com>
2020-04-09 20:29:49 -0400

    playFor func

--- refactoring/01.js
+++ refactoring/01.js
@@ -8,15 +8,15 @@ function statement(invoice, plays) {
         minimumFractionDigits: 2,
     }).format
     for (let perf of invoice.performances) {
-        const play = plays[perf.playID]
-        let thisAmount = amountFor(perf, play)
+        //const play = plays[perf.playID]
+        let thisAmount = amountFor(perf, playFor(perf))
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0)
         // add extra credit for every ten comedy attendees
-        if ('comedy' === play.type)
+        if ('comedy' === playFor(perf).type)
             volumeCredits += Math.floor(perf.audience / 5)
         // print line for this order
-        result += ` ${play.name}: ${format(thisAmount / 100)} (${
+        result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${
             perf.audience
         } seats)\n`
         totalAmount += thisAmount
@@ -25,9 +25,14 @@ function statement(invoice, plays) {
     result += `You earned ${volumeCredits} credits\n`
     return result
 }
+
+function playFor(aPerformance) {
+    return plays[aPerformance.playID]
+}
+
 function amountFor(aPerformance, play) {
     let result = 0
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
         case 'tragedy':
             result = 40000
             if (aPerformance.audience > 30) {
@@ -42,7 +47,7 @@ function amountFor(aPerformance, play) {
             result += 300 * aPerformance.audience
             break
         default:
-            throw new Error(`unknown type: ${play.type}`)
+            throw new Error(`unknown type: ${playFor(aPerformance).type}`)
     }
     return result
 }
