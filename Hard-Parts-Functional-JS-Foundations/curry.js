457961fa87ce1a5c4798c76090a8e029fb133d59
Wenlin Huang <jackhwl@hotmail.com>
2020-04-25 09:55:06 -0400

    ramda curry

--- Hard-Parts-Functional-JS-Foundations/curry.js
+++ Hard-Parts-Functional-JS-Foundations/curry.js
@@ -1,16 +1,29 @@
+const {curry} = require('ramda')
+
 const add = (x, y) => x + y
 
 const flip = f => (y, x) => f(x, y)
 
-const curry = f => x => y => f(x, y)
-
-const uncurry = f => (x, y) => f(x)(y)
+//const curry = f => x => y => f(x, y)
+const curry2 = function(f) {
+    return function(x) {
+        return function (y) {
+            return f(x, y)
+        }
+    }
+}
+ 
 
 const toPair = f => ([x, y]) => f(x, y)
+const toPair2 = function(f) {
+    return function([x, y]) {
+        return f(x, y)
+    }
+}
 
 const fromPair = f => (x, y) => f([x, y])
 
-const result = toPair(add)([1, 2])
+const result = toPair2(add)([1, 20])
 
 const result2 = fromPair(toPair(add))(3,2)
 
@@ -29,3 +42,18 @@ const modulo = curry((x, y) => y % x)
 const isOdd = modulo(2) // (2, y) => 2 % y
 const result4 = isOdd(3)
 console.log(result4)
+
+const filter = curry((f, xs) => xs.filter(f))
+
+const getOdds = filter(isOdd)
+
+const result5 = getOdds([1, 2, 3, 4, 5])
+
+console.log(result5)
+
+const replace = curry((regex, replacement, str) => str.replace(regex, replacement))
+
+const replaceVowels = replace(/[AEIOU]/ig, '!')
+
+const result6 = replaceVowels('Hey I have words')
+console.log(result6)
