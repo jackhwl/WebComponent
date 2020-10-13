88ca6dbbf84525e7c0941db20f32b3615266810e
Wenlin Huang <jackhwl@hotmail.com>
2020-10-13 10:33:04 -0400

    Function Signature Overloading

--- typescript-fundamentals/notes/2-function-basics.ts
+++ typescript-fundamentals/notes/2-function-basics.ts
@@ -43,29 +43,29 @@ console.log(sum(3, 4, 6)); // 13
 
 // (5) we can even provide multiple function signatures
 // "overload signatures"
-// function contactPeople(method: "email", ...people: HasEmail[]): void;
-// function contactPeople(method: "phone", ...people: HasPhoneNumber[]): void;
+function contactPeople(method: "email", ...people: HasEmail[]): void;
+function contactPeople(method: "phone", ...people: HasPhoneNumber[]): void;
 
-// "function implementation"
-// function contactPeople(
-//   method: "email" | "phone",
-//   ...people: (HasEmail | HasPhoneNumber)[]
-// ): void {
-//   if (method === "email") {
-//     (people as HasEmail[]).forEach(sendEmail);
-//   } else {
-//     (people as HasPhoneNumber[]).forEach(sendTextMessage);
-//   }
-// }
+"function implementation"
+function contactPeople(
+  method: "email" | "phone",
+  ...people: (HasEmail | HasPhoneNumber)[]
+): void {
+  if (method === "email") {
+    (people as HasEmail[]).forEach(sendEmail);
+  } else {
+    (people as HasPhoneNumber[]).forEach(sendTextMessage);
+  }
+}
 
 // ✅ email works
-// contactPeople("email", { name: "foo", email: "" });
+contactPeople("email", { name: "foo", email: "" });
 
 // ✅ phone works
-// contactPeople("phone", { name: "foo", phone: 12345678 });
+contactPeople("phone", { name: "foo", phone: 12345678 });
 
 // 🚨 mixing does not work
-// contactPeople("email", { name: "foo", phone: 12345678 });
+contactPeople("email", { name: "foo", phone: 12345678 });
 
 // (6) the lexical scope (this) of a function is part of its signature
 
