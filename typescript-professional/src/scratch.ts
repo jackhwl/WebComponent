3dd546fae8f51fa6b2c46e4ca7d25910da7d3b49
Wenlin Huang <jackhwl@hotmail.com>
2020-10-14 13:52:01 -0400

    remove my-lib sub folder

--- typescript-professional/src/scratch.ts
+++ typescript-professional/src/scratch.ts
@@ -1,24 +1,24 @@
-// @ts-expect-error
-const num1: number = 5 //'hello';
-// @ts-ignore
-const num2: number = 5 //'hello';
+// // @ts-expect-error
+// const num1: number = 5 //'hello';
+// // @ts-ignore
+// const num2: number = 5 //'hello';
 
-function somethingRisky() {}
+// function somethingRisky() {}
 
-function isError(err: any): err is Error {
-    return err instanceof Error;
-}
+// function isError(err: any): err is Error {
+//     return err instanceof Error;
+// }
 
-function assertIsError(err: any): asserts err is Error {
-    if (!(err instanceof Error)) throw new Error(`Not an error: ${err}`);
-}
-try {
-    somethingRisky();
-} catch( err: unknown) {
-    assertIsError(err);
-    // if (isError(err) {
-    //     console.log(err.stack);
-    // } else {
-        console.log(err);
-     // }
-}
+// function assertIsError(err: any): asserts err is Error {
+//     if (!(err instanceof Error)) throw new Error(`Not an error: ${err}`);
+// }
+// try {
+//     somethingRisky();
+// } catch( err: unknown) {
+//     assertIsError(err);
+//     // if (isError(err) {
+//     //     console.log(err.stack);
+//     // } else {
+//         console.log(err);
+//      // }
+// }
