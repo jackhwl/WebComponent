425532760eed306474fd5671b83992685124d650
Wenlin Huang <jackhwl@hotmail.com>
2020-10-17 17:38:17 -0400

    replace webcomponent hooks folder with default

---
+++ hooks/husky.local.sh
@@ -0,0 +1,6 @@
+# Created by Husky v4.3.0 (https://github.com/typicode/husky#readme)
+#   At: 10/16/2020, 11:36:56 AM
+#   From: undefined (https://github.com/typicode/husky#readme)
+
+packageManager=yarn
+cd "typescript-monorepos/"
